1aae9c2d22b522c2103f76f96ea683732456e1b0
kotaro <ks.s1230113@gmail.com>
2019-10-22 08:11:46 +0000

    add remember_me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_114437) do
+ActiveRecord::Schema.define(version: 2019_10_22_072101) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2019_09_25_114437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile"
+    t.string "remember_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
